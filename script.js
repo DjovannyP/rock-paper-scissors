2e4204c9274eaf4d67f3fdb7d5bcf16326a298d4
DjovannyP <pierredjovanny77@gmail.com>
2024-01-13 16:56:54 -0400

    Feat: condition to stop the game added
    My last commit about this project what about a condition to block the
    loop iteration through buttons. But I had to create an other repository
    on gitHub because I was not able to fix it. My css was not added and due
    to this, the website was not displayed as I wanted to.

---
+++ script.js
@@ -0,0 +1,72 @@
+
+//computer choice
+const arrayChoice = ["rock", "paper", "scissors"];
+
+//['Rock', 'Paper', 'Scissors]
+const buttons = document.querySelectorAll('button');
+
+function getComputerChoice(arr){
+ let choice = Math.floor(Math.random() * arr.length); 
+ return arrayChoice[choice];
+}
+//display
+const result = document.querySelector('.result-screen h1');
+const playerScoreDisplay = document.querySelector('#player-score p');
+const computerScoreDisplay = document.querySelector('#computer-score p');
+//player and computer selection
+let playerSelection;
+let computerSelection;
+let playerScore = 0;
+let computerScore = 0;
+
+function playRound(playerSelection, computerSelection){ 
+    if (playerSelection === "rock" && computerSelection === "paper") {
+        computerScore ++;
+        return "You lose! Paper beats rock ";
+        
+    } 
+    else if (playerSelection === "paper" && computerSelection === "rock") {
+        playerScore ++;
+        return "You win! Paper beats Rock";
+
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        playerScore ++;
+        return "You win! Rock beats Scissors";
+
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        computerScore ++;
+        return "You lose! Rock beats Scissors";
+
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        computerScore++;
+        return "You lose! Scissors beat Paper";
+
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        playerScore++;
+        return "You win! Scissors beat Paper";
+    } else {
+        return "It's a TIES!";
+    }
+}
+
+function gameOver(){
+    if(playerScore > computerScore){
+        return "Congratulation! You win.";
+    }else{
+        return " You lose! Take your revenge.";
+    }
+}
+
+for(let i = 0; i < buttons.length; i++){
+    buttons[i].addEventListener('click', (e) =>{
+        playerSelection = e.target.value;
+        computerSelection = getComputerChoice(arrayChoice);
+        result.textContent = playerScore === 5 || computerScore === 5 ? gameOver() : playRound(playerSelection, computerSelection);
+        playerScoreDisplay.textContent = playerScore;
+        computerScoreDisplay.textContent = computerScore;
+    });
+    
+    
+}
+
+    
